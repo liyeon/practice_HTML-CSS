13930bca0448ca65510a600f1e58a27afe20352a
liyeon <fldus612@gmail.com>
2021-02-13 23:20:36 +0900

    [ Feat ] 👑 Vanilla Js Challenge_day6
    - day5에 이어서 진행

--- 👑nomadcoder👑/day5/greeting.js
+++ 👑nomadcoder👑/day5/greeting.js
@@ -10,6 +10,28 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
       SHOWING_CN = "showing";
 
+function saveName(text){
+
+    localStorage.setItem(USER_LS, text)
+}
+
+function handleSubmit(event){
+    // event의 preventDefault
+    //  기본으로 실행되는 이벤트를 막아준다.
+    event.preventDefault();
+    const currentValue = input.value;
+    console.log(currentValue);
+    // paintGreeting으로 입력한 input 값을 보내준다.
+    paintGreeting(currentValue);
+    // 입력한 namedmf wjwkdgownsek.
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
@@ -20,6 +42,7 @@ function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null){
         // lacal stroge 가 없는 경우
+        askForName();
     } else {
         // 있는 경우
         paintGreeting(currentUser);
